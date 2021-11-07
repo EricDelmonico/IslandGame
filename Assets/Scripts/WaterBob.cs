a87b1e0c77e9b563653b6d015dcd52b271a06e2e
Eric Delmonico <ead1758@rit.edu>
2021-11-07 11:15:04 -0500

    Message in bottle bobs now

---
+++ Assets/Scripts/WaterBob.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class WaterBob : MonoBehaviour
+{
+    [SerializeField]
+    private float height = 0.1f;
+
+    [SerializeField]
+    private float period = 1;
+
+    private Vector3 initialPosition;
+    private float offset;
+
+    private void Awake()
+    {
+        initialPosition = transform.position;
+        offset = 1 - (Random.value * 2);
+    }
+
+    private void Update()
+    {
+        transform.position = initialPosition - Vector3.up * Mathf.Sin((Time.time + offset) * period) * height;
+    }
+}
