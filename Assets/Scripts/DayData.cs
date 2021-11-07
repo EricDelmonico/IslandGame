def9a86f5ab58cdce517d35f2263726e4f20776b
Eric Delmonico <ead1758@rit.edu>
2021-11-06 20:36:03 -0500

    Added objectives to day data

--- Assets/Scripts/DayData.cs
+++ Assets/Scripts/DayData.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,17 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "New Day", menuName = "Day")]
 public class DayData : ScriptableObject
 {
+    [Serializable]
+    public struct Objective
+    {
+        public string objectTag;
+        public int amountToCollect;
+    }
+
     public int day;
 
     public Text morningNote;
     public Text eveningNote;
+
+    public Objective[] objectives;
 }
